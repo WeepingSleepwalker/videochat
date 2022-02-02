29c10e2a37efbdc4fc9bb1b4140a3b052759d659
WeepingSleepwalker <88568965+WeepingSleepwalker@users.noreply.github.com>
2022-02-01 20:25:16 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Typography, AppBar } from '@material-ui/core';
+
+
+export const App = () => {
+  return (
+  <div>
+      <AppBar position="static" color= "inherit">
+        <Typography variant="h2" align="center">Video Chatter</Typography>
+      </AppBar>
+      {/* Video Player
+      Options --> Notifications */}
+  </div>
+  )};
+  export default App;
